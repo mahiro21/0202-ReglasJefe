cc3513c25cb76c95633bafa4ed690bc83c3ab88d
mario <mario@DESKTOP-4NRNOBI.home>
2019-10-04 19:14:22 +0200

    :tada: HolaMundo

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -22,6 +22,7 @@ package org.japo.java.main;
 public class Main {
     
     public static void main(String[] args){
-    System.out.println("Hola Mundo");
+    System.out.printf("Artículo 1. El jefe tiene la razón.%n" + "Artículo 2. El jefe SIEMPRE tiene la razón. %n" +  "Artículo 3. En caso de duda se aplicará el artículo 2.");
+    
     }
 }
